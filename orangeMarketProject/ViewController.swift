ce766e844586ae535b54bb580a595bec5463bbc8
Vladimir Lobanov <vladimirlobanov@Mac-mini-Alexander.local>
2017-01-13 15:43:08 +0300

    Initial Commit

---
+++ orangeMarketProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  orangeMarketProject
+//
+//  Created by Vladimir Lobanov on 13.01.17.
+//  Copyright © 2017 Vladimir Lobanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
